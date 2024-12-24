2e8ecd9e05a0bf46ad6fba02ac66de67eccfc170
Raymond Lei(雷菩宇) <ray810815@gmail.com>
2024-12-23 22:32:51 -0500

    spam.lan

---
+++ spam.fs
@@ -0,0 +1,4 @@
+[<EntryPoint>]
+let main argv =
+    printfn "Hello, World!"
+    0 // return an integer exit code
---
+++ spam.sql
@@ -0,0 +1,3 @@
+-- SQL does not have a direct way to print "Hello, World!". 
+-- However, you can select a constant string from a database.
+SELECT 'Hello, World!';
